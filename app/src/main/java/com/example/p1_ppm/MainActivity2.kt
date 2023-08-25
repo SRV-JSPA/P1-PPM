3d7e412572ea12ff4d2db5d48afb44d7fa6fd3f7
JSPA <per22318@uvg.edu.gt>
2023-08-24 20:56:50 -0600

    actualizaciones

--- app/src/main/java/com/example/p1_ppm/MainActivity2.kt
+++ app/src/main/java/com/example/p1_ppm/MainActivity2.kt
@@ -1,16 +1,14 @@
+
 package com.example.p1_ppm
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.p1_ppm.ui.theme.P1PPmTheme
+
+
+
+
+
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,4 +18,8 @@ class MainActivity2 : ComponentActivity() {
         }
     }
 }
+
+
+
+
 
