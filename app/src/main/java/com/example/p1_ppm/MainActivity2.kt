b2bd958cffea51bf17598f5e123f5ba5e78e324f
JSPA <per22318@uvg.edu.gt>
2023-08-24 21:04:21 -0600

    vista Log-in

--- app/src/main/java/com/example/p1_ppm/MainActivity2.kt
+++ app/src/main/java/com/example/p1_ppm/MainActivity2.kt
@@ -4,20 +4,78 @@ package com.example.p1_ppm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-
-
-
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            sesion()
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun sesion() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Iniciar sesión",
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        OutlinedTextField(
+            value = "Usuario",
+            onValueChange = {},
+            label = { Text(text = "Usuario") },
+            enabled = false,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        )
+
+        OutlinedTextField(
+            value = "Contraseña",
+            onValueChange = {},
+            label = { Text(text = "Contraseña") },
+            enabled = false,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = false
+        ) {
+            Text(text = "Iniciar sesión")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Previews() {
+    sesion()
+}
+
 
 
 
