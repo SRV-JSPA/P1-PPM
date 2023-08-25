257bbca8a8cbc1886f358167bf7ab7f64e5ae689
JSPA <per22318@uvg.edu.gt>
2023-08-24 19:45:11 -0600

    actualizaciones

--- app/src/main/java/com/example/p1_ppm/MainActivity2.kt
+++ app/src/main/java/com/example/p1_ppm/MainActivity2.kt
@@ -16,31 +16,8 @@ class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            P1PPmTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    P1PPmTheme {
-        Greeting("Android")
-    }
-}
