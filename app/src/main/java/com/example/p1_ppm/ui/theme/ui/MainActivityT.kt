a5d125f07b25ffd855095c3f734b83b9f55500a5
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-08-24 21:48:58 -0600

    Tutores

---
+++ app/src/main/java/com/example/p1_ppm/ui/theme/ui/MainActivityT.kt
@@ -0,0 +1,20 @@
+package com.example.p1_ppm.ui.theme.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+
+class MainActivityT : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+
+
+
+
+
+
